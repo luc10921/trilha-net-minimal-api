cee715120b5611dc9daab5e18d7f25e1b6c578bb
Lucas Kenji Uezu <kenjiuezu3@gmail.com>
2024-08-18 19:59:49 -0300

    Create db and seed it

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("SqlServerConnection")
+    );
+});
 
 app.MapGet("/", () => "Hello World! Pepe");
 
